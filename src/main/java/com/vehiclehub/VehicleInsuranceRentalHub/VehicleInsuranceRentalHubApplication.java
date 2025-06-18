39c48572c201bc1878205c39b25ed49b0c26fedb
hp <hp@172.26.58.66>
2025-06-18 15:11:38 +0530

    Adding first commit to add project to git

---
+++ src/main/java/com/vehiclehub/VehicleInsuranceRentalHub/VehicleInsuranceRentalHubApplication.java
@@ -0,0 +1,13 @@
+package com.vehiclehub.VehicleInsuranceRentalHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsuranceRentalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInsuranceRentalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehiclehub/VehicleInsuranceRentalHub/VehicleInsuranceRentalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiclehub.VehicleInsuranceRentalHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceRentalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
